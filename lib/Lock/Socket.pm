86f7cd4e2093c4bf13ce32fe001bf74b205e0d07
Mark Lawrence <nomad@null.net>
2014-09-13 14:45:53 +0200

    Unset release date

--- lib/Lock/Socket.pm
+++ lib/Lock/Socket.pm
@@ -162,7 +162,7 @@ Lock::Socket - application lock/mutex module based on sockets
 
 =head1 VERSION
 
-0.0.1_3 (2014-09-13) development release.
+0.0.1_3 (yyyy-mm-dd) development release.
 
 =head1 SYNOPSIS
 
